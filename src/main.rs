8191cf2e2b9fc40528a9c0a5cfb1b93e10994362
Mati <mrjudka@example.com>
2025-09-07 15:11:44 +0100

    start of problem 3

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,12 @@ fn main() {
     // let _problem_one: i32 = problem_one::solve(1000);
     // println!("Problem 1 solution = {_problem_one}");
 
-    let _problem_two = match problems::problem_two::solve(4000000, 2,1) {
-        Ok(_problem_two) => println!("{_problem_two}"),
-        Err(e) => println!("{}", e)
-    };
+    // let _problem_two = match problems::problem_two::solve(4000000, 2,1) {
+    //     Ok(_problem_two) => println!("{_problem_two}"),
+    //     Err(e) => println!("{}", e)
+    // };
+
+    let _problem_three = problems::problem_three::solve(600851475143);
     
 }
+//600851475143
--- src/problems/problem_three.rs
+++ src/problems/problem_three.rs
@@ -1,7 +1,61 @@
 //find the largest prime factor of a given number
 
+pub fn solve(number_to_analyse: u64){
 
-// pub fn solve(number_to_analyse: u32)-> u32{
+    let mut prime_vectors: Vec<u64> = Vec::new();
+    let mut prime_vectors_in_number: Vec<u64> =  Vec::new();
  
+    for n in 1..number_to_analyse/2{
 
-// }
+        
+
+        let mut is_prime_product:bool = false;
+
+        // need a faster prime finding algorithm
+        for prime in prime_vectors.iter(){
+            if *prime != 1 && n % prime == 0 {
+                is_prime_product = true;
+                break;
+            }
+        }
+
+        if is_prime_product {
+            continue;
+        }
+        
+        let mut is_not_prime: bool = false;
+        let mut multiples: Vec<u64> = Vec::new();
+        for i in 1.. n{
+            
+            if i % n == 0{
+                multiples.push(i);
+            }
+
+            if multiples.len() >= 2 {
+                is_not_prime = true;
+                break;
+            }
+        }
+        
+        if is_not_prime{
+            continue;
+        }
+        println!("{n}");
+
+        prime_vectors.push(n);
+
+        if number_to_analyse % n == 0 {
+
+            println!("prime product : {n}");
+            prime_vectors_in_number.push(n);
+        }
+    }
+
+    if prime_vectors_in_number.len() == 0{
+            println!("{number_to_analyse} is a prime")
+    }
+    else{
+        
+        println!("{number_to_analyse} is a prime composite") 
+    }
+}
